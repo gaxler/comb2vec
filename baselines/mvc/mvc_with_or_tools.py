4e9c23db053585f04bbe81812b255e6e55459397
Gregory <g.axler@gmail.com>
2018-04-07 18:19:59 +0300

    generate MVC solution with CP

---
+++ baselines/mvc/mvc_with_or_tools.py
@@ -0,0 +1,114 @@
+import _pickle as pkl
+import numpy as np
+from pathlib import Path
+
+from comb2vec.baselines.or_tools_base import ORToolsIntegerSolutionBase, IntegerProgramSolverFromGraph, \
+    graph_iterator_from_path
+
+
+class MVCSolutions(ORToolsIntegerSolutionBase):
+    pass
+
+
+class MvcStats(object):
+
+    def __init__(self, adj_matrix, solutions):
+        self._adj_matrix = adj_matrix
+        self._solutions = solutions
+
+    @property
+    def degrees(self):
+        return self._adj_matrix.sum(0)
+
+    @property
+    def mean_degree(self):
+        return np.mean(self.degrees)
+
+    @property
+    def std_degree(self):
+        return np.std(self.degrees)
+
+    @property
+    def wall_time(self):
+        return self._solutions[-1].wall_time
+
+    @property
+    def objective(self):
+        return self._solutions[-1].objective
+
+    def as_dict(self):
+        d = {'wall_time': self.wall_time, 'objective': self.objective}
+        return d
+
+
+class MinVertexCoverSolverFromGraph(IntegerProgramSolverFromGraph):
+
+    def __init__(self, graph):
+        super(MinVertexCoverSolverFromGraph, self).__init__(graph=graph)
+        self._adj_matrix = MinVertexCoverSolverFromGraph.numpy_adjecency_mat(graph.adjacency())
+
+        self.decision_vars = self.generate_decision_variables()
+        self.add_ge_sum_constraints()
+
+    def generate_decision_variables(self):
+        xs = {}
+        for vertex_num in self.graph.nodes:
+            xs[vertex_num] = self.solver.IntVar(0, 1, 'x%d' % vertex_num)
+        return xs
+
+    def add_ge_sum_constraints(self, sum_value=1):
+        for edge in self.graph.edges:
+            edge_decision_vars = (self.decision_vars[v] for v in edge)
+            self._add_ge_sum_constraint(edge_decision_vars, sum_value=sum_value)
+
+    def solve(self, step=1):
+        solver = self.solver
+
+        obj_expr = solver.IntVar(0, len(self.decision_vars), "obj_expr")
+        solver.Add(obj_expr == sum(self.decision_vars.values()))
+        objective = solver.Minimize(obj_expr, step)
+        decision_builder = solver.Phase(list(self.decision_vars.values()),
+                                        solver.CHOOSE_RANDOM,
+                                        solver.ASSIGN_RANDOM_VALUE)
+        # Create a solution collector.
+        collector = solver.AllSolutionCollector()
+        # Add the decision variables.
+        for v in self.decision_vars.values():
+            collector.Add(v)
+        # Add the objective.
+        collector.AddObjective(obj_expr)
+
+        init_time = solver.WallTime()
+        solver.Solve(decision_builder, [objective, collector])
+        solve_time = solver.WallTime() - init_time
+
+        solution_count = collector.SolutionCount()
+        solutions = [MVCSolutions(objective=collector.ObjectiveValue(i), wall_time=solve_time,
+                                  solution_vector=MinVertexCoverSolverFromGraph.solution_to_numpy_vector(
+                                      self.decision_vars, collector.Solution(i)))
+                     for i in range(solution_count)]
+        return MvcStats(adj_matrix=self._adj_matrix, solutions=solutions)
+
+
+if __name__ == '__main__':
+    it = graph_iterator_from_path('/home/gregory/projects/nips18/data/mvc/nodes_100_100')
+
+    save_path = Path('/home/gregory/projects/nips18/data/mvc/cp_solutions_100_100')
+
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
+    i = 0
+    for g, g_description in it:
+        i += 1
+        test_g = MinVertexCoverSolverFromGraph(graph=g)
+        stats = test_g.solve()
+        for idx, solution in enumerate(stats._solutions):
+            d = {'adj_mat': stats._adj_matrix, 'solution': solution.solution_vector,
+                 'objective': solution.objective, 'time': solution.wall_time, 'description': g_description,
+                 'degrees': stats.degrees, 'mu_deg': stats.mean_degree, 'sigma_deg': stats.std_degree}
+
+            with (save_path / ('cp_solution_%05d_%s.pkl' % (idx, g_description))).open('wb') as fp:
+                pkl.dump(d, fp)
+
+        print('(%04d) saved %d soluyions' % (i, len(stats._solutions)))
+        print('-----------------------\n')
---
+++ baselines/or_tools_base.py
@@ -0,0 +1,56 @@
+
+import networkx as nx
+import numpy as np
+
+from ortools.constraint_solver import pywrapcp
+from pathlib import Path
+import _pickle as pkl
+
+
+def graph_iterator_from_path(path, glob_str='*.pkl'):
+    path = Path(path)
+    for graph_pickle in path.glob(glob_str):
+        with graph_pickle.open('rb') as fp:
+            yield pkl.load(fp), graph_pickle.stem
+
+class ORToolsIntegerSolutionBase(object):
+
+    def __init__(self, wall_time, objective, solution_vector, **kwargs):
+        self.wall_time = wall_time
+        self.objective = objective
+        self.solution_vector = solution_vector
+
+
+class IntegerProgramSolverFromGraph(object):
+
+    def __init__(self, graph):
+        self.graph = graph
+
+        parameters = pywrapcp.Solver.DefaultSolverParameters()
+        solver = pywrapcp.Solver("MinVertexCover_CP", parameters)
+        self.solver = solver
+
+    def _add_ge_sum_constraint(self, iterable_of_variables, sum_value=1):
+        self.solver.Add(sum(iterable_of_variables) >= sum_value)
+
+    @staticmethod
+    def numpy_adjecency_mat(adj_item):
+        adj = dict(adj_item)
+        N = len(adj)
+        A = np.zeros((N, N))
+
+        for first_node in adj:
+            for second_node in adj[first_node]:
+                A[first_node, second_node] = 1
+
+        return A
+
+    @staticmethod
+    def solution_to_numpy_vector(optimization_vars_dict, solution):
+        vector_dim = len(optimization_vars_dict)
+        x = np.zeros(vector_dim, dtype=np.int32)
+
+        for v in optimization_vars_dict:
+            x[v] = solution.Value(optimization_vars_dict[v])
+
+        return x

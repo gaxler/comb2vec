560e231b7aa8519bd84014df4417461a24c06ce8
gaxler <g.axler@gmail.com>
2018-04-21 20:36:57 +0300

    Encoder with feasability loss In Progress: Debug and make sure all calculations as expected

--- feas_encoding.py
+++ feas_encoding.py
@@ -22,7 +22,7 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='enables CUDA training')
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
-parser.add_argument('--nodes', type=int, default=7, help='number of nodes in a graph')
+parser.add_argument('--nodes', type=int, default=20, help='number of nodes in a graph')
 parser.add_argument('--log-interval', type=int, default=1000, metavar='N',
                     help='how many batches to wait before logging training status')
 parser.add_argument('--gpus', type=str, default=None, help='CUDA_VISBLE_DEVICES setting')
@@ -53,10 +53,9 @@ num_nodes = args.nodes
 train_loader = get_loader('data_dir/mvc/cp_solutions_%d_%d' % (num_nodes, num_nodes), batch_size=args.batch_size,
                           shuffle=True)
 
-encoder = MLPEncoder(num_nodes, 32, 32)
-sol_classification = SolutionFaeture(feature_size=32, n_hid=32, n_out=1)
-# graph2vec = GaussianGraphVAE(num_nodes=num_nodes, hid_dim=num_nodes * 52, z_dim=num_nodes * 3,
-#                              enc_kwargs={'num_hidden': args.enc_hiddens}, dec_kwargs={'num_hidden': args.dec_hiddens})
+d = num_nodes
+encoder = MLPEncoder(num_nodes, d, d)
+sol_classification = SolutionFaeture(feature_size=d, n_hid=d, n_out=1)
 
 if args.cuda:
     encoder.cuda()
@@ -65,7 +64,8 @@ if args.cuda:
 for k, v in encoder.state_dict().items():
     print('%s: %s' % (k, v.type()))
 
-optimizer = optim.Adam(encoder.parameters(), lr=1e-3)
+learning_rate = 1e-2
+optimizer = optim.Adam(list(encoder.parameters()) + list(sol_classification.parameters()), lr=learning_rate)
 
 
 def adj_mat_to_tensors(off_diag: np.array, dtype=np.float32) -> (torch.FloatTensor, torch.FloatTensor):
@@ -93,15 +93,28 @@ def solution_is_cover(adj_mat, solution):
     return is_indepe_set
 
 
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+    lr = learning_rate * (0.1 ** (epoch // 10))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
 
 def train(epoch):
     encoder.train()
+    sol_classification.train()
+    adjust_learning_rate(optimizer, epoch)
     train_loss = 0
     valid_sols = 0
-    random_sol = Bernoulli(probs=torch.FloatTensor([0.5]))
+    # random_sol = Bernoulli(probs=torch.FloatTensor([0.5]))
     for idx, (adj_mat, solution) in enumerate(train_loader):
         rel_rec, rel_send = adj_mat_to_tensors(adj_mat.numpy())
-        sol_tensor = random_sol.sample_n(adj_mat.size(0)*num_nodes).view(-1, num_nodes)
+        if np.random.rand() < 0.45:
+             # sol_tensor = random_sol.sample_n(adj_mat.size(0)*num_nodes).view(-1, num_nodes)
+             sol_tensor = solution
+        else:
+            ss = solution.size(1)
+            sol_tensor = torch.stack([z[torch.randperm(ss)] for z in solution])
         inputs = torch.eye(num_nodes).unsqueeze(0).expand(adj_mat.size(0), num_nodes, num_nodes).contiguous()
         is_cover = solution_is_cover(adj_mat, sol_tensor).unsqueeze(1)
 
@@ -114,8 +127,8 @@ def train(epoch):
         sol_tensor = Variable(sol_tensor)
 
         if args.cuda:
-            rel_rec.cuda()
-            rel_send.cuda()
+            rel_rec = rel_rec.cuda()
+            rel_send = rel_send.cuda()
             inputs = inputs.cuda()
             is_cover = is_cover.cuda()
             sol_tensor = sol_tensor.cuda()
@@ -125,38 +138,24 @@ def train(epoch):
         sol_codes = torch.matmul(sol_tensor.unsqueeze(1), encoded_nodes).squeeze(1)
 
         logits = sol_classification(sol_codes)
-        loss = sol_classification.bce_loss(is_cover, logits)
+        loss = sol_classification.bce_loss(logits, is_cover)
 
         loss.backward()
-        train_loss += loss.data[0]
+        train_loss += loss.data[0]*adj_mat.size(0)
         optimizer.step()
         if idx % args.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, idx * len(adj_mat), len(train_loader.dataset),
                        100. * idx / len(train_loader),
-                       loss.data[0] / len(adj_mat)))
+                       loss.data[0]))
 
+    av_loss = train_loss / len(train_loader.dataset)
     print('====> Epoch: {} Average loss: {:.4f} Average valids: {:.4f}'.format(
-        epoch, train_loss / len(train_loader.dataset), valid_sols / len(train_loader.dataset)))
+        epoch, av_loss, valid_sols / len(train_loader.dataset)))
+
+    return av_loss
 
 
 if __name__ == '__main__':
-
     for epoch in range(1, args.epochs + 1):
-        train(epoch)
-
-        # sample = Variable(torch.randn(100, graph2vec.z_dim))
-        # if args.cuda:
-        #     sample = sample.cuda()
-        # sampled_adj_mat = graph2vec.decode(sample).cpu()
-        #
-        # for idx, th in enumerate((0.9,)):
-        #     sampled_adj_mat = sampled_adj_mat > th
-        #     sampled_adj_mat = sampled_adj_mat.view(-1, num_nodes, num_nodes)
-        #     mat = sampled_adj_mat.data.cpu().numpy()
-        #     print(mat.shape)
-        #     valids = np.mean([(np.allclose(m, m.T, atol=1e-8) and (np.count_nonzero(m) > 0)) for m in mat])
-        #     avg_ranks = np.mean(np.sum(mat, axis=2), axis=1)
-        #     print('(%d)[ %d ] %5.4f are valid matrices. %5.4f non zero. av range %4.2f' % (
-        #     idx + 1, int(th * 100), valids, np.count_nonzero(mat) / mat.size, np.mean(avg_ranks)))
-        # # print('%3.2f mean rank' % (np.mean(avg_ranks)))
+        av_loss = train(epoch)
